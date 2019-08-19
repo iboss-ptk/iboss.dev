d59ae3e7d90c5ac76815e523648c236a264917e1
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-08-19 08:34:20 +0700

    Clean up default manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `iboss.dev`,
+    description: `Iboss' blog and probably other random stuffs I wanna share`,
+    author: `@iboss-ptk`,
   },
   plugins: [
     `gatsby-plugin-mdx`,
@@ -26,8 +26,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `iboss.dev`,
+        short_name: `iboss.dev`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
